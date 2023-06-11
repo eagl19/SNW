aded2f0f63098ff2ef7120edc970b2f787cffd3e
eagl19 <eagl19@i.ua>
2023-06-11 20:44:17 +0300

    Update deploying.js

--- kubejs/server_scripts/create/deploying.js
+++ kubejs/server_scripts/create/deploying.js
@@ -1,9 +1,10 @@
 onEvent('recipes', event => {
 	const recipes=[
-		{INGREDIENTS : [ {item : CREATE_ITEMS.SHAFT}, 			{item : GEARS.STAINLESS_STEEL} ],			RESULTS: [{item : CREATE_ITEMS.COGWHEEL}], 			KEY : 'cogwheel'},
-		{INGREDIENTS : [ {item : CREATE_ITEMS.SHAFT}, 			{item : GREATE_GEARS.STAINLESS_STEEL} ],	RESULTS: [{item : CREATE_ITEMS.LARGE_COGWHEEL}], 	KEY : 'large_cogwheel'},
-		{INGREDIENTS : [ {item : CREATE_ITEMS.ANDESITE_CASING}, {item : INGOTS.C_BRASS} ],					RESULTS: [{item : CREATE_ITEMS.BRASS_CASING}], 		KEY : 'brass_casing'},
-		{INGREDIENTS : [ {item : CREATE_ITEMS.ANDESITE_CASING}, {item : INGOTS.M_COPPER} ],					RESULTS: [{item : CREATE_ITEMS.COPPER_CASING}], 	KEY : 'copper_casing'}
+		{INGREDIENTS : [ {item : CREATE_ITEMS.SHAFT}, 			{item : GEARS.STAINLESS_STEEL} ],				RESULTS: [{item : CREATE_ITEMS.COGWHEEL}], 			KEY : 'cogwheel'},
+		{INGREDIENTS : [ {item : CREATE_ITEMS.SHAFT}, 			{item : GREATE_GEARS.STAINLESS_STEEL} ],		RESULTS: [{item : CREATE_ITEMS.LARGE_COGWHEEL}], 	KEY : 'large_cogwheel'},
+		{INGREDIENTS : [ {item : CREATE_ITEMS.ANDESITE_CASING}, {item : INGOTS.C_BRASS} ],						RESULTS: [{item : CREATE_ITEMS.BRASS_CASING}], 		KEY : 'brass_casing'},
+		{INGREDIENTS : [ {item : CREATE_ITEMS.ANDESITE_CASING}, {item : INGOTS.M_COPPER} ],						RESULTS: [{item : CREATE_ITEMS.COPPER_CASING}], 	KEY : 'copper_casing'},
+		{INGREDIENTS : [ {item : SHEETS.C_IRON}, 				{item : CREATE_ITEMS.POLISHED_ROZE_QUARTZ} ],	RESULTS: [{item : CREATE_ITEMS.ELECTRON_TUBE}], 	KEY : 'electron_tube'}
 	]
 	
 	recipes.forEach(recipe=>{
